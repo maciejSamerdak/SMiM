a5d56261c8ced9991d7818f063f1925449a1c322
maciejSamerdak <sam.maciej@interia.pl>
2020-04-23 23:52:05 +0200

    Add improved mark placing mechanic

--- app/src/main/java/com/example/maciej/smim/MainActivity.kt
+++ app/src/main/java/com/example/maciej/smim/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.maciej.smim
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.view.View
 import android.widget.Button
 import android.widget.GridLayout
 import android.widget.LinearLayout
@@ -10,23 +11,30 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var game: Game
+    private lateinit var playerOneScoreView: TextView
+    private lateinit var playerTwoScoreView: TextView
+    private lateinit var currentPlayerNumberView: TextView
+    private lateinit var board: GridLayout
+    private lateinit var confirmationButton: Button
+    private var lastPickedField: Int = -1
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val playerOneScoreView = findViewById<TextView>(R.id.player_1_score)
-        val playerTwoScoreView = findViewById<TextView>(R.id.player_2_score)
-        val currentPlayerNumberView = findViewById<TextView>(R.id.player_number)
+        playerOneScoreView = findViewById(R.id.player_1_score)
+        playerTwoScoreView = findViewById(R.id.player_2_score)
+        currentPlayerNumberView = findViewById(R.id.player_number)
+        confirmationButton = findViewById(R.id.confirm)
 
-        val board = findViewById<GridLayout>(R.id.board)
+        board = findViewById(R.id.board)
 
-        var game = Game(board.rowCount,board.columnCount)
-        var currentScore = game.scoreCount
-        playerOneScoreView.text = currentScore[0].toString()
-        playerTwoScoreView.text = currentScore[1].toString()
-        currentPlayerNumberView.text = if (game.isPlayerOneTurn) "1" else "2"
+        game = Game(board.rowCount,board.columnCount)
+
+        refreshView()
+        changeButtonState(confirmationButton)
 
         val displayMetrics = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(displayMetrics)
@@ -42,36 +50,35 @@ class MainActivity : AppCompatActivity() {
 
             field.setOnClickListener {
                 // It might be best to handle all of this within the Game class but can't tell for sure
-                val playerMark = game.getPlayerMark()
-                val numberOfButton: Int = field.tag as Int
-                if(field.text.equals("")) {
-                    field.text = playerMark.symbol
-                    game.refreshFields(boardToArray(board))
-                    field.setTextColor(playerMark.color)
-
-                    if(game.checkFields(numberOfButton)){
-                        if(game.isPlayerOneTurn) game.updateScore(1)
-                        else game.updateScore(2)
-                        game.resetBoard()
-                        resetBoardView(board)
-                    }
-                    else if(game.isBoardFull()){
-                        println("It's a draw!")
-                        game.resetBoard()
-                        resetBoardView(board)
-                    }
-                    game.switchTurn()
-                    // This part is changing the display, so it can stay here. We might consider making a function out of it
-                    currentScore = game.scoreCount
-                    playerOneScoreView.text = currentScore[0].toString()
-                    playerTwoScoreView.text = currentScore[1].toString()
-                    currentPlayerNumberView.text = if (game.isPlayerOneTurn) "1" else "2"
-                }
+                placeMark(field, x)
             }
         }
     }
 
-    fun boardToArray(board: GridLayout): Array<Array<String>>{
+    fun placeMark(field: Button, index: Int){
+        val playerMark = game.getPlayerMark()
+        if(field.text.equals("")) {
+            field.text = playerMark.symbol
+            field.setTextColor(playerMark.color)
+            if(lastPickedField >= 0){
+                val previousField = board.getChildAt(lastPickedField) as Button
+                previousField.text = ""
+            }
+            else{
+                changeButtonState(confirmationButton)
+            }
+            lastPickedField = index
+        }
+    }
+
+    fun refreshView(){
+        val currentScore = game.scoreCount
+        playerOneScoreView.text = currentScore[0].toString()
+        playerTwoScoreView.text = currentScore[1].toString()
+        currentPlayerNumberView.text = if (game.isPlayerOneTurn) "1" else "2"
+    }
+
+    fun boardToArray(): Array<Array<String>>{
         var fields = Array(board.rowCount,{Array(board.columnCount,{""})})
         for(i in 0 until board.rowCount){
             for(j in 0 until board.columnCount){
@@ -81,11 +88,34 @@ class MainActivity : AppCompatActivity() {
         return fields
     }
 
-    fun resetBoardView(board: GridLayout){
+    fun resetBoardView(){
         for ( x in 0 until board.childCount){
             val child = board.getChildAt(x)
             (child as Button).text = ""
         }
     }
 
+    fun confirmMove(view: View){
+        game.refreshFields(boardToArray())
+        if(game.checkFields(lastPickedField)){
+            if(game.isPlayerOneTurn) game.updateScore(1)
+            else game.updateScore(2)
+            game.resetBoard()
+            resetBoardView()
+        }
+        else if(game.isBoardFull()){
+            println("It's a draw!")
+            game.resetBoard()
+            resetBoardView()
+        }
+        game.switchTurn()
+        lastPickedField = -1
+        changeButtonState(view as Button)
+        refreshView()
+    }
+
+    fun changeButtonState(button: Button){
+        button.isEnabled = !button.isEnabled
+        button.isClickable = !button.isClickable
+    }
 }
